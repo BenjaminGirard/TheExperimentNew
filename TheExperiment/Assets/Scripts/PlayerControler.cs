15458e2b845170fbcb7528c2ad6bdb9e26411525
Riadh Meghenem <riadh.meghenem@epitech.eu>
2019-04-09 11:11:42 +0900

    Game Base

---
+++ TheExperiment/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class PlayerControler : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public Grid grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
+            Debug.Log(grid.name);
+            Debug.Log(coordinate);
+
+            GameObject go = (GameObject)Instantiate(Resources.Load("crate_0"));
+            go.transform.position = coordinate;
+            // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+            // if (hit)
+            // {
+            //     Debug.Log(hit.collider.gameObject.name);
+            // }
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
+    }
+}
