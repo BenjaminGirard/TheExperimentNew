43c6bb6585091b42f84c658f7a3b202ff35c860d
bibz <s.vidal3131@gmail.com>
2019-05-23 19:59:14 +0900

    MainMenu added

--- TheExperiment/Assets/DeathManager.cs
+++ TheExperiment/Assets/DeathManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathManager : MonoBehaviour
 {
@@ -14,9 +15,6 @@ public class DeathManager : MonoBehaviour
     void Update()
     {
         if (GetComponent<HealthManager>().IsDead)
-            Debug.Log("dead boy");
- // SceneManager.LoadScene ();
-            
-
+            SceneManager.LoadScene(0);
     }
 }
